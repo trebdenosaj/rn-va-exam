9f2770c05abe29fa6fd0ca25783e8cdcef43e962
trebdenosaj <ejestevez26@gmail.com>
2020-10-15 18:45:28 +0800

    refactor: add initial folder structure

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text
+} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <SafeAreaView>
+      <Text>Root Directory</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
